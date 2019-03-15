ca95836e2bf939935a0a908486cadc16ddd36135
andyqtchen <andyqtchen@tencent.com>
2019-03-15 19:18:13 +0800

    全屏显示

--- app/src/main/java/com/kesar/jetpackgank/base/BaseActivity.java
+++ app/src/main/java/com/kesar/jetpackgank/base/BaseActivity.java
@@ -1,10 +1,15 @@
 package com.kesar.jetpackgank.base;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
 
 import androidx.annotation.LayoutRes;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import androidx.databinding.ViewDataBinding;
@@ -22,14 +27,14 @@ public abstract class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mViewDataBinding = DataBindingUtil.setContentView(this, getLayoutId());
+        initContentView();
         initData();
         initView();
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if(item.getItemId() == android.R.id.home) {
+        if (item.getItemId() == android.R.id.home) {
             onBackPressed();
         }
         return super.onOptionsItemSelected(item);
@@ -42,6 +47,65 @@ public abstract class BaseActivity extends AppCompatActivity {
         return (M) mViewDataBinding;
     }
 
-    protected void initData(){}
-    protected void initView(){}
+    public void initContentView() {
+        mViewDataBinding = DataBindingUtil.setContentView(this, getLayoutId());
+    }
+
+    protected void initData() {
+    }
+
+    protected void initView() {
+    }
+
+
+    public void setFullScreen(boolean isShowStatusBar, boolean isShowNavigationBar) {
+        View decorView = getWindow().getDecorView();
+        int uiOptions = decorView.getSystemUiVisibility();
+
+        /* （上）状态栏相关 */
+        if (!isShowStatusBar) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                uiOptions |= View.SYSTEM_UI_FLAG_FULLSCREEN; // 隐藏状态栏
+            }
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                uiOptions |= View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN; // 布局全屏，如果有状态栏就显示在状态栏下面
+            }
+        }
+
+        /* （下）导航栏相关 */
+
+        if (!isShowNavigationBar) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                uiOptions |= View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION; // 布局全屏，如果有导航栏就显示在导航栏下面
+            }
+
+            uiOptions |= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;  // 隐藏导航栏
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            uiOptions |= View.SYSTEM_UI_FLAG_LAYOUT_STABLE;  // 隐藏状态栏或者导航栏时布局不变化，该留空的就留空
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            uiOptions |= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;  // 透明化的状态栏或者导航栏？
+        }
+
+        decorView.setSystemUiVisibility(uiOptions);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+
+        setNavigationStatusColor(Color.TRANSPARENT);
+    }
+
+    public void setNavigationStatusColor(int color) {
+        //VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP
+        if (Build.VERSION.SDK_INT >= 21) {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            getWindow().setNavigationBarColor(color);
+            getWindow().setStatusBarColor(color);
+        }
+    }
 }
